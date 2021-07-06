e410de40be7fd69cae5c53fce38751ce0339c077
dannyg919 <dannyg@mit.edu>
2021-07-06 17:04:41 -0400

    User can log in to account/Stay logged in

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("vQKiMqJUWobOMwzcDgr4K4JFJtjGe7xMrSlV1jyz")
+                .clientKey("DcmpUzxnIoxVLqPliJW8YVyQqVdngN1ujFG4JgMk")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
