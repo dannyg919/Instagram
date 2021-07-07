e47e85d0b604b17e9840001a9b8daa98210a0123
dannyg919 <dannyg@mit.edu>
2021-07-07 17:59:47 -0400

    User can swipe to refresh the feed

--- app/src/main/java/com/example/instagram/FeedActivity.java
+++ app/src/main/java/com/example/instagram/FeedActivity.java
@@ -3,6 +3,7 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,8 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +21,7 @@ public class FeedActivity extends AppCompatActivity {
     protected List<Post> allPosts;
     protected RecyclerView rvPosts;
     protected PostsAdapter postAdapter;
+    protected SwipeRefreshLayout swipeContainer;
 
 
     @Override
@@ -25,13 +29,29 @@ public class FeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         rvPosts = findViewById(R.id.rvPosts);
         allPosts = new ArrayList<>();
         postAdapter = new PostsAdapter(this,allPosts);
 
-
         rvPosts.setAdapter(postAdapter);
         rvPosts.setLayoutManager(new LinearLayoutManager(this));
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchFeedAsync(0);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
 
         queryPosts();
     }
@@ -59,6 +79,17 @@ public class FeedActivity extends AppCompatActivity {
                 postAdapter.notifyDataSetChanged();
             }
         });
+    }
+
+    public void fetchFeedAsync(int page) {
+
+                // Remember to CLEAR OUT old items before appending in the new ones
+        postAdapter.clear();
+                // ...the data has come back, add new items to your adapter...
+        queryPosts();
+                // Now we call setRefreshing(false) to signal refresh has finished
+        swipeContainer.setRefreshing(false);
+
     }
 
 }
