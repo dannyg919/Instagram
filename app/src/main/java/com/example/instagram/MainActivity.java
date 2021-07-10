21580b76b4953e8028d8a4fd4ba1d0e5181a839f
dannyg919 <dannyg@mit.edu>
2021-07-09 20:38:00 -0400

    Final Polish

---
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.instagram;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.instagram.fragments.CaptureFragment;
+import com.example.instagram.fragments.FeedFragment;
+import com.example.instagram.fragments.ProfileFragment;
+import com.example.instagram.models.BitmapScaler;
+import com.example.instagram.models.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG ="MainActivity";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.hide();
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        // define your fragments here
+        final Fragment homeFragment = new FeedFragment();
+        final Fragment captureFragment = new CaptureFragment();
+        final Fragment profileFragment = new ProfileFragment();
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fragment = homeFragment;
+                        break;
+                    case R.id.capture:
+                        fragment = captureFragment;
+                        break;
+                    case R.id.profile:
+                    default:
+                        fragment = profileFragment;
+                        break;
+
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout){
+            ParseUser.logOut();
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
